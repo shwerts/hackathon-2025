9404c14756b81508f90f43b4b1b3e349ace3f2e0
shwerts <abidzhanovtemirlan@gmail.com>
2025-07-23 21:26:20 +0500

    add main navigation

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,21 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+    <div class="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+      <div class="flex items-center gap-2">
+        <img src="./assets/777.jpg" alt="EduPulse Logo" class="h-8 w-auto">
+        <span class="text-xl font-bold text-blue-600">EduPulse</span>
+      </div>
+      <nav class="flex gap-6 font-medium text-gray-700">
+        <RouterLink to="/" class="hover:text-blue-600 transition">Главная</RouterLink>
+        <!-- <RouterLink to="/surveys" class="hover:text-blue-600 transition">Опросы</RouterLink> -->
+        <RouterLink to="/account" class="hover:text-blue-600 transition">Личный кабинет</RouterLink>
       </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
+  <main class="p-6 max-w-screen-xl mx-auto">
     <TheWelcome />
   </main>
 </template>
