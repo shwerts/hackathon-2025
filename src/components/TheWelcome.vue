4bd653b0668e739c2635fcd933f853b1361ae0f2
shwerts <abidzhanovtemirlan@gmail.com>
2025-07-25 23:32:14 +0500

    English

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,24 +1,9 @@
-<script setup>
-// import WelcomeItem from './WelcomeItem.vue'
-// import DocumentationIcon from './icons/IconDocumentation.vue'
-// import ToolingIcon from './icons/IconTooling.vue'
-// import EcosystemIcon from './icons/IconEcosystem.vue'
-// import CommunityIcon from './icons/IconCommunity.vue'
-// import SupportIcon from './icons/IconSupport.vue'
-
-// const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md');
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold mb-6">Добро пожаловать!</h1>
-  <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+  <h1 class="text-3xl text-center font-bold mb-6">Welcome!</h1>
+  <div class="grid grid-cols-1 justify-items-center gap-6">
     <div class="bg-white rounded-2xl p-6 shadow-md">
       <h2 class="text-xl font-bold mb-2">Student Feedback Survey</h2>
       <p class="text-sm text-gray-600">24 of 30 Responses</p>
-    </div>
-    <div class="bg-white rounded-2xl p-6 shadow-md">
-      <h2 class="text-xl font-bold mb-2">Midterm Evaluation</h2>
-      <p class="text-sm text-gray-600">Apr. 5, 2024</p>
     </div>
   </div>
 </template>
